2c49646476dc2926a2d9187ef70c1998479af302
Mats Lindblad <mats@wiral.se>
2011-10-28 14:22:54 +0200

    Started project for new homepage

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SlipsnisseSe::Application.config.secret_token = '81ece2a643781067c873ea012ddefd02f58f388369cbbd98808aeb064fa971442afafa5c03a6e870d15e5bfbc3f2a200e3723a34d9c5656bca571b9891c22e83'
